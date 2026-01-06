7c2f03beaac07f3d24095ccf839144b113f86a35
izwan61 <191495262+izwanGit@users.noreply.github.com>
2026-01-06 21:22:40 +0800

    security: add firebaseConfig example template

---
+++ firebaseConfig.example.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "beruangapp-d832c.firebaseapp.com",
+  projectId: "beruangapp-d832c",
+  storageBucket: "beruangapp-d832c.firebasestorage.app",
+  messagingSenderId: "720523475593",
+  appId: "YOUR_APP_ID"
+};
